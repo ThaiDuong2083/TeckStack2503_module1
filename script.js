0915fa580af14d24f8919800fe592f1236086108
Thai Duong <leduong2083@gmail.com>
2025-05-05 11:20:19 +0700

    add animation

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 document.querySelector("input[type='date']").value = new Date().toISOString().slice(0, 10);
+function quantityBook(value) {
+    let quantityElement = document.querySelector(".quantity-booking");
+    let input = parseInt(quantityElement.textContent);
+
+    input = value === "+" ? input + 1 : value === "-" ? input - 1 : input;
+    input = input < 1 ? 1 : input;
+
+    quantityElement.textContent = input;
+}
